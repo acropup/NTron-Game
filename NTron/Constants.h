0b1b960a3df1c5b7bd30c0c963a022f19256e3ac
Shane Burgess <acropup@users.noreply.github.com>
2016-03-24 10:48:02 -0700

    Constants cleanup and FenceButton logic
    Made Player's btnFence an actual Button struct, to be treated like all
    the others. Slightly less efficient here, but more analogous to when we
    put button control onto another arduino with UART.

--- NTron/Constants.h
+++ NTron/Constants.h
@@ -4,21 +4,27 @@
 #define NUM_LEDS_PER_STRIP 256
 #define NUM_STRIPS 3
 
+#define WIDTH  32
+#define HEIGHT 24
+
 #define FENCECOLOUR      CRGB::Blue
 #define BGCOLOUR         CRGB::Black //Background colour
 #define PLAYERCOLOUR     CRGB::Green
 #define ROCKETCOLOUR     CRGB::Orange
 #define ROCKETFADECOLOUR CRGB::Red
 
-#define WIDTH 32
-#define HEIGHT 24
+#define POWERUP_HUE_SPEED 30 //Speed of powerup rainbow animation
 
-#define MAX_TWEENS 256 //Maximum number of pixels that can be tweened simultaneously
-#define MAX_POWERUPS 32
-#define POWERUP_HUE_SPEED 30
-
-#define MAX_ROCKETS 32
+#define MAX_TWEENS    256 //Maximum number of pixels that can be tweened simultaneously
+#define MAX_POWERUPS   32
+#define MAX_ROCKETS    32
 #define MAX_EXPLOSIONS 32
+
+//Power and powerup constants. Power storage is in uint8_t, so max value per player is 255
+#define FENCE_COST        2
+#define ROCKET_COST      16
+#define POWERUP_VALUE    16
+#define POWER_REGEN_RATE 50 //Percent of a power point regenerated per frame
 
 #define DEBOUNCE_MS 40
 
--- NTron/Player.h
+++ NTron/Player.h
@@ -5,21 +5,17 @@
 #include "Button.h"
 #include "Rocket.h"
 
-#define FENCE_COST  2
-#define ROCKET_COST 16
-
 typedef struct Player_ {
   Button  btnL;    //Button that turns player Left (CCW)
   Button  btnR;    //Button that turns player Right (CW)
   Button  btnFire; //Button that fires rockets
-  uint8_t btnFencePin; //Button that leaves fences behind player
-  bool isFencing;  //Player is actively leaving fences along their trail
+  Button  btnFence;//Button that leaves fences behind player
   bool isAlive;    //Player is still alive
   int8_t x;  //Position on screen
   int8_t y;
   int8_t dx; //Direction traveling. Either dx or dy should be 0.
   int8_t dy;
-  uint8_t power; //Power level for using special ability. 16=1px Fence costs 2, powerup gives 16, auto-regen 1 per 2 frames.
+  uint8_t power; //Power level for using special ability. Rocket costs 16, 1px Fence costs 2, powerup gives 16, auto-regen 1 per 2 frames. Config in Constants.h.
 } Player;
 
 static const uint8_t NUMPLAYERS = 2;
@@ -30,7 +26,6 @@ uint8_t powerRegenCnt = 0;   //When this gets to 100, give all players 1 power
 
 void resetPlayer(Player& p, int8_t posX, int8_t posY, int8_t dx, int8_t dy){
   p.isAlive = true;
-  p.isFencing = false;
   p.power = 0;
   p.x = posX;
   p.y = posY;
@@ -39,8 +34,7 @@ void resetPlayer(Player& p, int8_t posX, int8_t posY, int8_t dx, int8_t dy){
 }
 
 Player& initPlayer(uint8_t pid, uint8_t leftPin, uint8_t rightPin, uint8_t fencePin, uint8_t firePin) {
-  players[pid] = { CreateButton(leftPin), CreateButton(rightPin), CreateButton(firePin), fencePin, false, true, 0, 0, 1, 0, 0 };
-  pinMode(fencePin, INPUT_PULLUP);
+  players[pid] = { CreateButton(leftPin), CreateButton(rightPin), CreateButton(firePin), CreateButton(fencePin), true, 0, 0, 1, 0, 0 };
   return players[pid];
 }
 
@@ -49,7 +43,7 @@ inline Player& getPlayer(uint8_t pid){
 }
 
 void checkButtons(Player& p){
-  p.isFencing |= (digitalRead(p.btnFencePin) == LOW);
+  Update(p.btnFence);
   Update(p.btnFire);
   Update(p.btnL);
   Update(p.btnR);
@@ -63,26 +57,28 @@ void checkButtons(){
   }
 }
 
-/* Checks if player has pressed the isFencing button,
- * and if they have enough power to lay a fence.
- * If so, consumes power and returns true.
- */
+/*Checks if player has pressed the isFencing button,
+  and if they have enough power to lay a fence.
+  If so, consumes power and returns true. */
 bool isPlayerFencing(Player& p){
-  if(p.isFencing && p.power > 1) {
+  Button &b = p.btnFence;
+  if(b.wasPressed && p.power >= FENCE_COST) {
     p.power-=FENCE_COST;
+    b.wasPressed = (b.isPressed == LOW); //Clear wasPressed only if button has been released
     return true;
   }
   return false;
 }
 
-//Lays a fence if Player is holding down the isFencing button, and they have enough power to afford it
+/*Lays a fence if Player is holding down the isFencing button, 
+  and they have enough power to afford it. */
 void maybeLayFence(CRGB leds[], Player& p) {
-    if(isPlayerFencing(p)) {
-      addPixelTween(tweenPixelTo(leds[XY(p.x, p.y)], FENCECOLOUR));
-    }
-    else {
-      addPixelTween(tweenPixelTo(leds[XY(p.x, p.y)], BGCOLOUR));
-    }
+  if(isPlayerFencing(p)) {
+    addPixelTween(tweenPixelTo(leds[XY(p.x, p.y)], FENCECOLOUR));
+  }
+  else {
+    addPixelTween(tweenPixelTo(leds[XY(p.x, p.y)], BGCOLOUR));
+  }
 }
 
 //Fires a rocket if Player pressed the Fire button this frame
@@ -141,7 +137,6 @@ inline void moveAndWrap(int8_t& x, int8_t& y, int8_t dx, int8_t dy) {
   if(y == -1)   y = HEIGHT-3;
   if(x == WIDTH) x = 0;
   if(y == HEIGHT-2) y = 0;
-  
 }
 
 /* Updates player position by moving player in direction dx or dy. */
@@ -150,23 +145,23 @@ void updatePlayerPosition(Player& p){
 }
 
 void moveRandom(uint8_t pid) {
-    players[pid].x = random(WIDTH);
-    players[pid].y = random(HEIGHT - 2); //height minus bottom bar
-    players[pid].dx = 0;
-    players[pid].dy = 0;
-    switch(random(4)) {
-      case 0: players[pid].dx = 1; break;
-      case 1: players[pid].dx = -1; break;
-      case 2: players[pid].dy = 1; break;
-      case 3: players[pid].dy = -1; break;
-    }
+  players[pid].x = random(WIDTH);
+  players[pid].y = random(HEIGHT - 2); //height minus bottom bar
+  players[pid].dx = 0;
+  players[pid].dy = 0;
+  switch(random(4)) {
+    case 0: players[pid].dx = 1; break;
+    case 1: players[pid].dx = -1; break;
+    case 2: players[pid].dy = 1; break;
+    case 3: players[pid].dy = -1; break;
+  }
 }
 
 //Call this once per frame before checking if any players have collided
 void updatePlayers(CRGB leds[]){
   //Player power regenerates slowly over time
   uint8_t regenAmt = 0;
-  powerRegenCnt += powerRegenRate;
+  powerRegenCnt += POWER_REGEN_RATE;
   if(powerRegenCnt >= 100) {
     powerRegenCnt -= 100;
     regenAmt = 1;
@@ -190,9 +185,9 @@ void updatePlayers(CRGB leds[]){
 }
 
 void applyPowerup(Player& p) {
-  p.power+=16;
+  p.power+=POWERUP_VALUE;
   //Check for overflow
-  if(p.power < 16) p.power = 255;
+  if(p.power < POWERUP_VALUE) p.power = 255;
 }
 
 /*To be called after the players are moved. Checks for player-to-player collisions,
